334f0c8f37fc8a6a3f1e321487b5a7c54910319e
yazar-propel <yazararabaths@propelsoft.in>
2023-10-05 12:46:19 +0530

    icon removed

--- category.php
+++ category.php
@@ -22,8 +22,7 @@ $pid = intval($_GET['pid']);
 
 									while ($row = mysqli_fetch_array($sql)) {
 										?>
-										<a href="sub-category.php?scid=<?php echo $row['id']; ?>" class="dropdown-toggle"><i
-												class="icon fa fa-desktop fa-fw"></i>
+										<a href="sub-category.php?scid=<?php echo $row['id']; ?>" class="dropdown-toggle">
 											<?php echo $row['subcategory']; ?>
 										</a>
 									<?php } ?>

bbd9836716b5cfb0663f4e766b6a70433b67b1d6
dhanaraj <dhanraj@propelsoft.in>
2023-10-25 15:43:02 +0530

    .

--- admin/insert-avail.php
+++ admin/insert-avail.php
@@ -7,7 +7,7 @@ if (strlen($_SESSION['alogin']) == 0) {
 } else {
 	$pid = intval($_GET['id']);
 	if (isset($_POST['submit'])) {
-		$prodAvail = $_POST['prodAvail'];
+		$prodAvail = ($_POST['prodOldAvail']) ? $_POST['prodOldAvail'] + $_POST['prodAvail'] : $_POST['prodAvail'];
 
 		$sql = mysqli_query($con, "UPDATE products SET prod_avail='$prodAvail' WHERE id='$pid'");
 
@@ -15,7 +15,7 @@ if (strlen($_SESSION['alogin']) == 0) {
 	}
 
 
-	?>
+?>
 	<!DOCTYPE html>
 	<html lang="en">
 
@@ -29,7 +29,9 @@ if (strlen($_SESSION['alogin']) == 0) {
 		<link type="text/css" href="images/icons/css/font-awesome.css" rel="stylesheet">
 		<link type="text/css" href='css/opensans.css' rel='stylesheet'>
 		<script src="assets/js/nicEdit-latest.js" type="text/javascript"></script>
-		<script type="text/javascript">bkLib.onDomLoaded(nicEditors.allTextAreas);</script>
+		<script type="text/javascript">
+			bkLib.onDomLoaded(nicEditors.allTextAreas);
+		</script>
 
 	</head>
 
@@ -65,73 +67,67 @@ if (strlen($_SESSION['alogin']) == 0) {
 												<?php echo htmlentities($_SESSION['msg']); ?>
 												<?php echo htmlentities($_SESSION['msg'] = ""); ?>
 											</div>
-										<?php }
+									<?php }
 									} ?>
 
 									<br />
 
-									<form class="form-horizontal row-fluid" name="insertavail" method="post"
-										enctype="multipart/form-data">
+									<form class="form-horizontal row-fluid" name="insertavail" method="post" enctype="multipart/form-data">
 
-                                        <?php
+										<?php
 
 										$query = mysqli_query($con, "select products.*,category.categoryName as catname,subcategory.subcategory as subcatname from products join category on category.id=products.category join subcategory on subcategory.id=products.subCategory where products.id='$pid'");
 										while ($row = mysqli_fetch_array($query)) {
 
 
 
-											?>
+										?>
 
-										<div class="control-group">
-											<label class="control-label" for="basicinput">Category <span>*</span></label>
-											<div class="controls">
-												<input type="text" name="category" placeholder="Enter category Name"
-													class="span8 tip" value="<?php echo $row['catname']; ?>" readonly required>
+											<div class="control-group">
+												<label class="control-label" for="basicinput">Category <span>*</span></label>
+												<div class="controls">
+													<input type="text" name="category" placeholder="Enter category Name" class="span8 tip" value="<?php echo $row['catname']; ?>" readonly required>
+												</div>
 											</div>
-										</div>
 
 
-										<div class="control-group">
-											<label class="control-label" for="basicinput">Sub Category <span>*</span></label>
-											<div class="controls">
-												<input type="text" name="subcategory" placeholder="Enter subcategory Name"
-													class="span8 tip" value="<?php echo $row['subcatname']; ?>" readonly required>
+											<div class="control-group">
+												<label class="control-label" for="basicinput">Sub Category <span>*</span></label>
+												<div class="controls">
+													<input type="text" name="subcategory" placeholder="Enter subcategory Name" class="span8 tip" value="<?php echo $row['subcatname']; ?>" readonly required>
+												</div>
 											</div>
-										</div>
 
 
-										<div class="control-group">
-											<label class="control-label" for="basicinput">Product Name <span>*</span></label>
-											<div class="controls">
-												<input type="text" name="productName" placeholder="Enter Product Name"
-                                                    value="<?php echo htmlentities($row['productName']); ?>" 
-													class="span8 tip" readonly required>
+											<div class="control-group">
+												<label class="control-label" for="basicinput">Product Name <span>*</span></label>
+												<div class="controls">
+													<input type="text" name="productName" placeholder="Enter Product Name" value="<?php echo htmlentities($row['productName']); ?>" class="span8 tip" readonly required>
+												</div>
 											</div>
-										</div>
 
-										<div class="control-group">
-											<label class="control-label" for="basicinput">Product Company <span>*</span></label>
-											<div class="controls">
-												<input type="text" name="productCompany"
-                                                    value="<?php echo htmlentities($row['productCompany']); ?>"
-													placeholder="Enter Product Comapny Name" class="span8 tip" readonly required>
+											<div class="control-group">
+												<label class="control-label" for="basicinput">Product Company <span>*</span></label>
+												<div class="controls">
+													<input type="text" name="productCompany" value="<?php echo htmlentities($row['productCompany']); ?>" placeholder="Enter Product Comapny Name" class="span8 tip" readonly required>
+												</div>
 											</div>
-										</div>
-										<div class="control-group">
-											<label class="control-label" for="basicinput">Kg and Nos <span>*</span></label>
-											<div class="controls">
-												<input type="text"
-													onkeypress="return event.charCode >= 48 && event.charCode <= 57"
-													name="prodAvail" placeholder="Enter Kg and Nos"
-													value="<?php echo htmlentities($row['prod_avail']); ?>"
-													class="span8 tip" required>
+											<div class="control-group">
+												<label class="control-label" for="basicinput">Current Available Kg Or Nos <span>*</span></label>
+												<div class="controls">
+													<input type="text" onkeypress="return event.charCode >= 48 && event.charCode <= 57" name="prodOldAvail" placeholder="0" value="<?php echo htmlentities($row['prod_avail']); ?>" class="span8 tip" readonly>
+												</div>
 											</div>
-										</div>
-                                        <?php } ?>
+											<div class="control-group">
+												<label class="control-label" for="basicinput">Kg or Nos <span>*</span></label>
+												<div class="controls">
+													<input type="text" onkeypress="return event.charCode >= 48 && event.charCode <= 57" name="prodAvail" placeholder="Enter Kg or Nos" value="" class="span8 tip" required>
+												</div>
+											</div>
+										<?php } ?>
 										<div class="control-group">
 											<div class="controls">
-                                                <input class="btn" type="button" value="Back"
-													onclick="window.location.href = 'product-avail.php'" />
+												<input class="btn" type="button" value="Back" onclick="window.location.href = 'product-avail.php'" />
 												<button type="submit" name="submit" class="btn">Update</button>
 											</div>
 										</div>
@@ -152,9 +148,9 @@ if (strlen($_SESSION['alogin']) == 0) {
 		<?php include('include/footer.php'); ?>
 
 		<script src="scripts/jquery-1.9.1.min.js" type="text/javascript"></script>
-        
+
 		<script>
-            
+
 		</script>
 	</body>
 <?php } ?>
